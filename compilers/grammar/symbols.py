5a698227fe38f3e35ea074c352795f4bea772321
Victor Giovannoni <vernalhav@usp.br>
2022-10-28 20:09:37 -0300

    Add is_terminal/nonterminal type guards

--- compilers/grammar/symbols.py
+++ compilers/grammar/symbols.py
@@ -1,4 +1,14 @@
-from .terminals import Terminal
+from typing import TypeGuard
+
 from .nonterminals import Nonterminal
+from .terminals import Terminal
 
 Symbol = Nonterminal | Terminal
+
+
+def is_nonterminal(symbol: Symbol) -> TypeGuard[Nonterminal]:
+    return isinstance(symbol, Nonterminal)
+
+
+def is_terminal(symbol: Symbol) -> TypeGuard[Terminal]:
+    return isinstance(symbol, Terminal)
