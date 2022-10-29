5f20e22fc532ccd1a0c2ea31e9f4a5c152cc5499
Victor Giovannoni <vernalhav@usp.br>
2022-10-29 10:36:53 -0300

    Make first_sets an attribute

--- compilers/grammar/grammar.py
+++ compilers/grammar/grammar.py
@@ -9,47 +9,41 @@ FirstSet = set[Terminal]
 
 
 class Grammar:
-    productions: Mapping[Nonterminal, Production]
+    _productions: Mapping[Nonterminal, Production]
 
     def __init__(self, productions: Iterable[Production]) -> None:
-        self.productions = {
+        self._productions = {
             production.nonterminal: production for production in productions
+        }
+        self._first_sets: dict[Nonterminal, FirstSet] = {
+            nonterminal: set() for nonterminal in self.nonterminals
         }
         # TODO: validate production invariants
         # TODO: make productions frozendict
 
     def get_production(self, nonterminal: Nonterminal) -> Production:
-        return self.productions[nonterminal]
+        return self._productions[nonterminal]
 
     @property
     def nonterminals(self) -> Iterable[Nonterminal]:
         # If productions is frozen, this can be a regular attribute
-        return self.productions.keys()
+        return self._productions.keys()
 
     def get_first(self, nonterminal: Nonterminal) -> FirstSet:
-
-        first_sets: dict[Nonterminal, FirstSet] = {
-            nonterminal: set() for nonterminal in self.nonterminals
-        }
 
         changed = True
         while changed:
             changed = False
             for current_nonterminal in self.nonterminals:
-                new_symbols = self._get_new_first_pass(current_nonterminal, first_sets)
-                first_sets[current_nonterminal].update(new_symbols)
+                new_symbols = self._get_new_first_pass(current_nonterminal)
+                self._first_sets[current_nonterminal].update(new_symbols)
 
                 if len(new_symbols) > 0:
                     changed = True
 
-        return first_sets[nonterminal]
+        return self._first_sets[nonterminal]
 
-    def _get_new_first_pass(
-        self,
-        nonterminal: Nonterminal,
-        current_first_sets: Mapping[Nonterminal, FirstSet],
-    ) -> FirstSet:
-
+    def _get_new_first_pass(self, nonterminal: Nonterminal) -> FirstSet:
         first: FirstSet = set()
 
         for derivation in self.get_production(nonterminal).derivations:
@@ -57,6 +51,6 @@ class Grammar:
             if is_terminal(head):
                 first.add(head)
             elif is_nonterminal(head):
-                first.update(current_first_sets[head])
+                first.update(self._first_sets[head])
 
-        return first - current_first_sets[nonterminal]
+        return first - self._first_sets[nonterminal]
