ecb8df8a368352b4cd75e86f45ac9dbb5c80250d
Victor Giovannoni <vernalhav@usp.br>
2022-11-19 18:11:09 -0300

    Add LR1 item class and test

--- compilers/parser/lr_items.py
+++ compilers/parser/lr_items.py
@@ -1,11 +1,14 @@
-from __future__ import annotations
-
+import dataclasses
 from dataclasses import dataclass
+from typing import TypeVar
 
 from compilers.grammar.productions import ProductionLine
+from compilers.grammar.terminals import Terminal
+
+Self = TypeVar("Self", bound="LRItem")  # TODO: Remove once mypy supports Self
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, kw_only=True)
 class LRItem:
     production: ProductionLine
     _stack_position: int = 0
@@ -14,9 +17,14 @@ class LRItem:
     def can_reduce(self) -> bool:
         return self._stack_position == len(self.production.derivation)
 
-    def next(self) -> LRItem:
+    def next(self: Self) -> Self:
         if self.can_reduce:
             raise ValueError(
                 "Cannot advance an LR Item that is at the end of the production"
             )
-        return LRItem(self.production, self._stack_position + 1)
+        return dataclasses.replace(self, _stack_position=self._stack_position + 1)
+
+
+@dataclass(frozen=True, kw_only=True)
+class LR1Item(LRItem):
+    lookahead: Terminal
--- tests/test_lr_items.py
+++ tests/test_lr_items.py
@@ -2,7 +2,7 @@ import pytest
 
 from compilers.grammar import Nonterminal, Terminal
 from compilers.grammar.productions import Production
-from compilers.parser.lr_item import LRItem
+from compilers.parser.lr_items import LR1Item, LRItem
 
 
 def test_cannot_advance_finished_item() -> None:
@@ -12,7 +12,7 @@ def test_cannot_advance_finished_item() -> None:
 
     production = Production(A, [(a, b)])
     production_line, *_ = production.derivations
-    item = LRItem(production_line)
+    item = LRItem(production=production_line)
 
     for _ in production_line.derivation:
         item = item.next()
@@ -26,7 +26,20 @@ def test_cannot_advance_null_production() -> None:
     production = Production(A, [()])
     production_line, *_ = production.derivations
 
-    item = LRItem(production_line)
+    item = LRItem(production=production_line)
 
     with pytest.raises(ValueError):
         item.next()
+
+
+def test_advance_lr1_preserves_lookahead() -> None:
+    A = Nonterminal("A")
+    a = Terminal("a")
+    b = Terminal("b")
+
+    production = Production(A, [(a, b)])
+    production_line, *_ = production.derivations
+    item = LR1Item(production=production_line, lookahead=b)
+
+    item = item.next()
+    assert item.lookahead == b
