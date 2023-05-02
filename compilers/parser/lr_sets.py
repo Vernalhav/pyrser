a72b35761fefcbd9a8544d82f9003d48a1ca3316
Victor Giovannoni <vernalhav@usp.br>
2023-05-01 23:55:51 -0300

    Add initial LR sets and closure

---
+++ compilers/parser/lr_sets.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from collections import deque
+from dataclasses import dataclass, field
+from typing import AbstractSet, Generic, Iterable, TypeVar
+
+from compilers.grammar.grammar import Grammar
+from compilers.grammar.symbols import is_nonterminal
+from compilers.parser.lr_items import LRItem
+
+LRItemType = TypeVar("LRItemType", bound=LRItem)
+
+
+@dataclass(frozen=True)
+class LRSet(Generic[LRItemType]):
+    kernel: frozenset[LRItemType]
+    nonkernel: frozenset[LRItemType] = field(default_factory=frozenset)
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, LRSet):
+            return self.kernel == other.kernel
+        if not isinstance(other, AbstractSet):
+            return False
+        return self.kernel == other
+
+    def __hash__(self) -> int:
+        """Custom hashing method that excludes nonkernel items."""
+        return hash(self.kernel)
+
+    @staticmethod
+    def from_items(
+        kernel: Iterable[LRItemType], nonkernel: Iterable[LRItemType] | None = None
+    ) -> LRSet[LRItemType]:
+        """Convenience method to pass any iterable to the constructor
+        and avoid having to explicitly convert the iterables to frozensets.
+        """
+        if nonkernel is not None:
+            return LRSet(frozenset(kernel), frozenset(nonkernel))
+        return LRSet(frozenset(kernel))
+
+def closure(lr_set: LRSet[LRItem], g: Grammar) -> LRSet[LRItem]:
+    previous_set: LRSet[LRItem] | None = None
+    current_set = lr_set
+
+    while (
+        previous_set is None
+        or previous_set.nonkernel != current_set.nonkernel
+    ):
+        nonkernel_items = set(current_set.nonkernel)
+
+        for item in current_set.kernel | current_set.nonkernel:
+            if not item.complete and is_nonterminal(nonterminal := item.next_symbol):
+                for line in g.get_production(nonterminal).derivations:
+                    nonkernel_items.add(LRItem(line))
+
+        previous_set = current_set
+        current_set = LRSet(lr_set.kernel, frozenset(nonkernel_items))
+
+    return current_set
+
+
+def get_initial_lr_item(g: Grammar) -> LRItem:
+    """Assumes `g` is an augmented grammar."""
+    initial_line, *_ = g.get_production(g.start_symbol).derivations
+    return LRItem(initial_line)
+
---
+++ tests/test_lr.py
@@ -0,0 +1,95 @@
+from compilers.grammar import Grammar
+from compilers.grammar.nonterminals import Nonterminal
+from compilers.grammar.productions import Production, ProductionLine
+from compilers.grammar.terminals import Terminal
+from compilers.parser.lr_items import LRItem
+from compilers.parser.lr_sets import LRSet, closure, compute_lr_sets
+
+
+def test_lr_sets_compare_kernel_only() -> None:
+    S_prime = Nonterminal("S'")
+    S = Nonterminal("S")
+    a = Terminal("a")
+    b = Terminal("b")
+
+    start_item = LRItem(ProductionLine(S_prime, (S,)))
+    s_to_a_item = LRItem(ProductionLine(S, (a,)))
+    s_to_b_item = LRItem(ProductionLine(S, (b,)))
+
+    kernel_only = LRSet.from_items({start_item})
+    different_kernel = LRSet.from_items({start_item, s_to_a_item})
+    with_nonkernel = LRSet.from_items({start_item}, {s_to_a_item, s_to_b_item})
+
+    assert kernel_only == with_nonkernel
+    assert len({kernel_only, with_nonkernel}) == 1
+    assert len({kernel_only, different_kernel}) == 2
+
+
+def test_lr_set_closure_includes_all_kernel_items() -> None:
+    # S -> A
+    # A -> aC | B
+    # B -> b
+    # C -> c
+
+    S = Nonterminal("S")
+    A = Nonterminal("A")
+    B = Nonterminal("B")
+    C = Nonterminal("C")
+    a = Terminal("a")
+    b = Terminal("b")
+    c = Terminal("c")
+
+    s_production = Production(S, (A,))
+    a_production = Production(A, ((a, C), B))
+    b_production = Production(B, (b,))
+    c_production = Production(C, (c,))
+
+    start_item = LRItem(ProductionLine(S, (A,)))
+    a_to_a_item = LRItem(ProductionLine(A, (a, C)))
+    a_to_b_item = LRItem(ProductionLine(A, (B,)))
+    b_to_b_item = LRItem(ProductionLine(B, (b,)))
+    c_to_c_item = LRItem(ProductionLine(C, (c,)))
+
+    g = Grammar((s_production, a_production, b_production, c_production), S)
+
+    lr_set = LRSet.from_items({start_item, a_to_a_item.next()})
+
+    assert closure(lr_set, g).nonkernel == {
+        a_to_a_item,
+        a_to_b_item,
+        b_to_b_item,
+        c_to_c_item,
+    }
+
+
+def test_lr_set_closure_doesnt_propagate_nonterminal() -> None:
+    # S -> A
+    # A -> aC | B
+    # B -> b
+    # C -> c
+
+    S = Nonterminal("S")
+    A = Nonterminal("A")
+    B = Nonterminal("B")
+    C = Nonterminal("C")
+    a = Terminal("a")
+    b = Terminal("b")
+    c = Terminal("c")
+
+    s_production = Production(S, (A,))
+    a_production = Production(A, ((a, C), B))
+    b_production = Production(B, (b,))
+    c_production = Production(C, (c,))
+
+    start_item = LRItem(ProductionLine(S, (A,)))
+    a_to_a_item = LRItem(ProductionLine(A, (a, C)))
+    a_to_b_item = LRItem(ProductionLine(A, (B,)))
+    b_to_b_item = LRItem(ProductionLine(B, (b,)))
+
+    g = Grammar((s_production, a_production, b_production, c_production), S)
+
+    lr_set = LRSet.from_items({start_item})
+
+    assert closure(lr_set, g).nonkernel == {a_to_a_item, a_to_b_item, b_to_b_item}
+
+
