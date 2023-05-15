56374013f7b564e65b8bf1ac190ab670040e0c3d
Victor Giovannoni <vernalhav@usp.br>
2023-05-14 23:26:03 -0300

    Move closure tests to lr_set test suite

--- tests/test_lr_sets.py
+++ tests/test_lr_sets.py
@@ -1,5 +1,6 @@
+from compilers.grammar.grammar import Grammar
 from compilers.grammar.nonterminals import Nonterminal
-from compilers.grammar.productions import ProductionLine
+from compilers.grammar.productions import Production, ProductionLine
 from compilers.grammar.terminals import Terminal
 from compilers.parser.lr_items import LRItem
 from compilers.parser.lr_sets import LR0Set
@@ -22,3 +23,91 @@ def test_lr_sets_compare_kernel_only() -> None:
     assert kernel_only == with_nonkernel
     assert len({kernel_only, with_nonkernel}) == 1
     assert len({kernel_only, different_kernel}) == 2
+
+
+def test_lr_set_closure_includes_all_kernel_items() -> None:
+    # S -> A
+    # A -> aC | B
+    # B -> b
+    # C -> c
+
+    S = Nonterminal("S")
+    A = Nonterminal("A")
+    B = Nonterminal("B")
+    C = Nonterminal("C")
+    a = Terminal("a")
+    b = Terminal("b")
+    c = Terminal("c")
+
+    s_production = Production(S, [A])
+    a_production = Production(A, [(a, C), B])
+    b_production = Production(B, [b])
+    c_production = Production(C, [c])
+
+    start_item = LRItem(ProductionLine(S, (A,)))
+    a_to_a_item = LRItem(ProductionLine(A, (a, C)))
+    a_to_b_item = LRItem(ProductionLine(A, (B,)))
+    b_to_b_item = LRItem(ProductionLine(B, (b,)))
+    c_to_c_item = LRItem(ProductionLine(C, (c,)))
+
+    g = Grammar((s_production, a_production, b_production, c_production), S)
+
+    lr_set = LR0Set({start_item, a_to_a_item.next()})
+
+    assert lr_set.closure(g).nonkernel == {
+        a_to_a_item,
+        a_to_b_item,
+        b_to_b_item,
+        c_to_c_item,
+    }
+
+
+def test_lr_set_closure_doesnt_propagate_nonterminal() -> None:
+    # S -> A
+    # A -> aC | B
+    # B -> b
+    # C -> c
+
+    S = Nonterminal("S")
+    A = Nonterminal("A")
+    B = Nonterminal("B")
+    C = Nonterminal("C")
+    a = Terminal("a")
+    b = Terminal("b")
+    c = Terminal("c")
+
+    s_production = Production(S, [A])
+    a_production = Production(A, [(a, C), B])
+    b_production = Production(B, [b])
+    c_production = Production(C, [c])
+
+    start_item = LRItem(ProductionLine(S, (A,)))
+    a_to_a_item = LRItem(ProductionLine(A, (a, C)))
+    a_to_b_item = LRItem(ProductionLine(A, (B,)))
+    b_to_b_item = LRItem(ProductionLine(B, (b,)))
+
+    g = Grammar((s_production, a_production, b_production, c_production), S)
+
+    lr_set = LR0Set({start_item})
+
+    assert lr_set.closure(g).nonkernel == {a_to_a_item, a_to_b_item, b_to_b_item}
+
+
+def test_lr_set_closure_creates_empty_item() -> None:
+    # S -> A
+    # A -> <empty string>
+
+    S = Nonterminal("S")
+    A = Nonterminal("A")
+
+    s_production = Production(S, [A])
+    a_production = Production(A, [()])
+
+    start_item = LRItem(ProductionLine(S, (A,)))
+    empty_item = LRItem(ProductionLine(A, ()))
+
+    g = Grammar((s_production, a_production), S)
+
+    lr_set = LR0Set({start_item})
+
+    assert lr_set.closure(g).nonkernel == {empty_item}
