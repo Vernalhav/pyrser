1edbfc2f1821192f7d9e52402ac893db48cad035
Victor Giovannoni <vernalhav@usp.br>
2023-05-24 18:23:51 -0300

    Simplify LALR automata tests

--- tests/test_lalr_automata.py
+++ tests/test_lalr_automata.py
@@ -1,6 +1,6 @@
 from compilers.grammar.grammar import Grammar
 from compilers.grammar.nonterminals import Nonterminal
-from compilers.grammar.productions import Production, ProductionLine
+from compilers.grammar.productions import Production
 from compilers.grammar.terminals import Terminal
 from compilers.parser.lalr_automata import (
     LALRAutomata,
@@ -8,7 +8,8 @@ from compilers.parser.lalr_automata import (
     determine_lookahead_relationships,
     get_end_of_chain,
 )
-from compilers.parser.lr_items import LR1Item, LRItem
+from compilers.parser.lr_automata import LRAutomata
+from compilers.parser.lr_items import items_from_production
 from compilers.parser.lr_sets import LR0Set, LR1Set
 from compilers.utils import GroupedDict
 
@@ -27,20 +28,17 @@ def test_lookahead_relationships() -> None:
     eq = Terminal("=")
     id = Terminal("id")
 
-    sp_production = Production(Sp, [S])
-    s_production = Production(S, [(L, eq, R), R])
-    l_production = Production(L, [(times, R), id])
-    r_production = Production(R, [L])
+    sp_prod = Production(Sp, [S])
+    s_prod = Production(S, [(L, eq, R), R])
+    l_prod = Production(L, [(times, R), id])
+    r_prod = Production(R, [L])
 
-    start_item = LRItem(ProductionLine(Sp, (S,)))
-    s_to_l = LRItem(ProductionLine(S, (L, eq, R)))
-    s_to_r = LRItem(ProductionLine(S, (R,)))
-    l_to_r = LRItem(ProductionLine(L, (times, R)))
-    l_to_r = LRItem(ProductionLine(L, (times, R)))
-    l_to_id = LRItem(ProductionLine(L, (id,)))
-    r_to_l = LRItem(ProductionLine(R, (L,)))
+    (start_item,) = items_from_production(sp_prod)
+    s_to_l, s_to_r = items_from_production(s_prod)
+    l_to_r, l_to_id = items_from_production(l_prod)
+    (r_to_l,) = items_from_production(r_prod)
 
-    g = Grammar((sp_production, s_production, l_production, r_production), Sp)
+    g = Grammar((sp_prod, s_prod, l_prod, r_prod), Sp)
     state = LR0Set({start_item, s_to_l.next()})
 
     expected = LookaheadRelationships(
@@ -80,24 +78,21 @@ def test_lalr_automata_creation_pointer_grammar() -> None:
     eq = Terminal("=")
     id = Terminal("id")
 
-    sp_production = Production(Sp, [S])
-    s_production = Production(S, [(L, eq, R), R])
-    l_production = Production(L, [(times, R), id])
-    r_production = Production(R, [L])
+    sp_prod = Production(Sp, [S])
+    s_prod = Production(S, [(L, eq, R), R])
+    l_prod = Production(L, [(times, R), id])
+    r_prod = Production(R, [L])
 
-    g = Grammar((sp_production, s_production, l_production, r_production), Sp)
+    g = Grammar((sp_prod, s_prod, l_prod, r_prod), Sp)
     end_of_chain = get_end_of_chain(g)
 
-    start_item = LR1Item(ProductionLine(Sp, (S,)), end_of_chain)
-    s_to_l = LR1Item(ProductionLine(S, (L, eq, R)), end_of_chain)
-    s_to_r = LR1Item(ProductionLine(S, (R,)), end_of_chain)
-    l_to_r = LR1Item(ProductionLine(L, (times, R)), end_of_chain)
-    l_to_r = LR1Item(ProductionLine(L, (times, R)), end_of_chain)
-    l_to_id = LR1Item(ProductionLine(L, (id,)), end_of_chain)
-    r_to_l = LR1Item(ProductionLine(R, (L,)), end_of_chain)
-    r_to_l_eq = LR1Item(ProductionLine(R, (L,)), eq)
-    l_to_r_eq = LR1Item(ProductionLine(L, (times, R)), eq)
-    l_to_id_eq = LR1Item(ProductionLine(L, (id,)), eq)
+    (start_item,) = items_from_production(sp_prod, end_of_chain)
+    s_to_l, s_to_r = items_from_production(s_prod, end_of_chain)
+    l_to_r, l_to_id = items_from_production(l_prod, end_of_chain)
+    (r_to_l,) = items_from_production(r_prod, end_of_chain)
+    r_to_l_eq = r_to_l.to_lr1(eq)
+    l_to_r_eq = l_to_r.to_lr1(eq)
+    l_to_id_eq = l_to_id.to_lr1(eq)
 
     states = (
         LR1Set({start_item}),
@@ -112,6 +107,8 @@ def test_lalr_automata_creation_pointer_grammar() -> None:
         LR1Set({s_to_l.next().next().next()}),
     )
 
-    g = Grammar((sp_production, s_production, l_production, r_production), Sp)
     automata = LALRAutomata(g)
+
+    # Assumes transitions are OK since they should be copied from LR0 automata
+    assert automata.transition_count == LRAutomata(g).transition_count
     assert automata.states == set(states)
