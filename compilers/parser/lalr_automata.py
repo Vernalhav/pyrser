9741612469e97a222c856be4a9d6fdd096d3be1d
Victor Giovannoni <vernalhav@usp.br>
2023-05-24 11:17:05 -0300

    Add LR1 automata and tests

--- compilers/parser/lalr_automata.py
+++ compilers/parser/lalr_automata.py
@@ -1,14 +1,118 @@
+from collections import defaultdict
 from typing import NamedTuple
 
 from compilers.grammar.grammar import Grammar
 from compilers.grammar.symbols import Symbol
 from compilers.grammar.terminals import Terminal
+from compilers.parser.lr_automata import LRAutomata
 from compilers.parser.lr_items import LRItem
-from compilers.utils import GroupedDefaultDict, GroupedDict
+from compilers.parser.lr_sets import LR0Set, LR1Set, LRSet
+from compilers.utils import GroupedDefaultDict, GroupedDict, flatten
 
-GeneratedLookaheads = GroupedDict[Symbol, LRItem, set[Symbol]]
+StateLookaheads = GroupedDict[LRSet, LRItem, set[Terminal]]
+PropagationTable = GroupedDict[LR0Set, LRItem, dict[LR0Set, set[LRItem]]]
+GeneratedLookaheads = GroupedDict[Symbol, LRItem, set[Terminal]]
 PropagatedLookaheads = GroupedDict[Symbol, LRItem, set[LRItem]]
 
+
+class LALRAutomata:
+    grammar: Grammar
+    states: set[LR1Set]
+    start_state: LR1Set
+    _transitions: dict[tuple[LR1Set, Symbol], LR1Set]
+
+    def __init__(self, g: Grammar) -> None:
+        self.grammar = g
+        self._compute_states_and_transitions()
+
+    @property
+    def transition_count(self) -> int:
+        return len(self._transitions)
+
+    def get_transition(self, state: LR1Set, symbol: Symbol) -> LR1Set:
+        return self._transitions[(state, symbol)]
+
+    def _compute_states_and_transitions(self) -> None:
+        lr0_automata = LRAutomata(self.grammar)
+        lookaheads = self._propagate_lookaheads(lr0_automata)
+
+        lr0_to_lr1_states: dict[LR0Set, LR1Set] = {}
+
+        self.states = set()
+        for lr0_state in lr0_automata.states:
+            kernel_items = (
+                lr0_item.to_lr1(lookahead)
+                for lr0_item in lr0_state.kernel
+                for lookahead in lookaheads[lr0_state, lr0_item]
+            )
+            lr1_state = LR1Set(kernel_items)
+            self.states.add(lr1_state)
+            lr0_to_lr1_states[lr0_state] = lr1_state
+
+        self.start_state = lr0_to_lr1_states[lr0_automata.start_state]
+
+        self._transitions = {}
+        for lr0_start, symbol, lr0_end in lr0_automata.transitions:
+            start, end = lr0_to_lr1_states[lr0_start], lr0_to_lr1_states[lr0_end]
+            self._transitions[(start, symbol)] = end
+
+    def _propagate_lookaheads(self, automata: LRAutomata) -> StateLookaheads:
+        lookaheads, table = self._compute_initial_lookaheads_and_propagations(automata)
+
+        changed = True
+        while changed:
+            changed = False
+
+            for start_state, start_item, propagations in table.flatten():
+                propagated_lookaheads = lookaheads[start_state, start_item]
+                for target_state, target_item in flatten(propagations):
+                    set_to_update = lookaheads[target_state, target_item]
+                    changed |= not (propagated_lookaheads <= set_to_update)
+                    set_to_update |= propagated_lookaheads
+
+        return lookaheads
+
+    def _compute_initial_lookaheads_and_propagations(
+        self, automata: LRAutomata
+    ) -> tuple[StateLookaheads, PropagationTable]:
+        lookaheads: StateLookaheads = GroupedDefaultDict(set)
+        propagations: PropagationTable = GroupedDefaultDict(lambda: defaultdict(set))
+
+        start_state = automata.start_state
+        start_item, *_ = start_state.kernel
+        end_of_chain = get_end_of_chain(self.grammar)
+        lookaheads[start_state, start_item].add(end_of_chain)
+
+        for state in automata.states:
+            generated, propagated = determine_lookahead_relationships(
+                state, self.grammar
+            )
+            self._add_generated(state, generated, lookaheads, automata)
+            self._add_propagated(state, propagated, propagations, automata)
+
+        return lookaheads, propagations
+
+    def _add_generated(
+        self,
+        state: LR0Set,
+        generated: GeneratedLookaheads,
+        lookaheads: StateLookaheads,
+        automata: LRAutomata,
+    ) -> None:
+        for symbol, item, generated_lookaheads in generated.flatten():
+            target_state = automata.get_transition(state, symbol)
+            lookaheads[target_state, item] |= generated_lookaheads
+
+    def _add_propagated(
+        self,
+        state: LR0Set,
+        propagated: PropagatedLookaheads,
+        propagation_table: PropagationTable,
+        automata: LRAutomata,
+    ) -> None:
+        for symbol, item, propagated_items in propagated.flatten():
+            target_state = automata.get_transition(state, symbol)
+            propagation_table[state, item][target_state] |= propagated_items
 
 
 class LookaheadRelationships(NamedTuple):
@@ -31,10 +135,18 @@ class LookaheadRelationships(NamedTuple):
         return is_equal
 
 
+def get_dummy(g: Grammar) -> Terminal:
+    return Terminal("#")  # TODO: Dynamically change value to not conflict with grammar
+
+
+def get_end_of_chain(g: Grammar) -> Terminal:
+    return Terminal("$")  # TODO: Dynamically change value to not conflict with grammar
+
+
 def determine_lookahead_relationships(
     state: LR0Set, g: Grammar
 ) -> LookaheadRelationships:
-    dummy = Terminal("#")  # TODO: Dynamically change value to not conflict with grammar
+    dummy = get_dummy(g)
     relationships = LookaheadRelationships(
         propagated=GroupedDefaultDict(set),
         generated=GroupedDefaultDict(set),
@@ -49,8 +161,8 @@ def determine_lookahead_relationships(
             next_symbol = item.next_symbol
             next_item = item.next().to_lr0()
             if item.lookahead == dummy:
-                relationships.propagated[next_symbol][kernel_item].add(next_item)
+                relationships.propagated[next_symbol, kernel_item].add(next_item)
             else:
-                relationships.generated[next_symbol][next_item].add(item.lookahead)
+                relationships.generated[next_symbol, next_item].add(item.lookahead)
 
     return relationships
--- tests/test_lalr_automata.py
+++ tests/test_lalr_automata.py
@@ -3,6 +3,7 @@ from compilers.grammar.nonterminals import Nonterminal
 from compilers.grammar.productions import Production, ProductionLine
 from compilers.grammar.terminals import Terminal
 from compilers.parser.lalr_automata import (
+    LALRAutomata,
     LookaheadRelationships,
     determine_lookahead_relationships,
 )
@@ -62,3 +63,52 @@ def test_lookahead_relationships() -> None:
 
     relationships = determine_lookahead_relationships(state, g)
     assert relationships == expected
+
+
+def test_lalr_automata_creation_pointer_grammar() -> None:
+    # S' -> S
+    # S -> L = R | R
+    # L -> *R | id
+    # R -> L
+
+    Sp = Nonterminal("S'")
+    S = Nonterminal("S")
+    L = Nonterminal("L")
+    R = Nonterminal("R")
+    times = Terminal("*")
+    eq = Terminal("=")
+    id = Terminal("id")
+    end_of_chain = Terminal("$")
+
+    sp_production = Production(Sp, [S])
+    s_production = Production(S, [(L, eq, R), R])
+    l_production = Production(L, [(times, R), id])
+    r_production = Production(R, [L])
+
+    start_item = LR1Item(ProductionLine(Sp, (S,)), end_of_chain)
+    s_to_l = LR1Item(ProductionLine(S, (L, eq, R)), end_of_chain)
+    s_to_r = LR1Item(ProductionLine(S, (R,)), end_of_chain)
+    l_to_r = LR1Item(ProductionLine(L, (times, R)), end_of_chain)
+    l_to_r = LR1Item(ProductionLine(L, (times, R)), end_of_chain)
+    l_to_id = LR1Item(ProductionLine(L, (id,)), end_of_chain)
+    r_to_l = LR1Item(ProductionLine(R, (L,)), end_of_chain)
+    r_to_l_eq = LR1Item(ProductionLine(R, (L,)), eq)
+    l_to_r_eq = LR1Item(ProductionLine(L, (times, R)), eq)
+    l_to_id_eq = LR1Item(ProductionLine(L, (id,)), eq)
+
+    states = (
+        LR1Set({start_item}),
+        LR1Set({start_item.next()}),
+        LR1Set({s_to_l.next(), r_to_l.next()}),
+        LR1Set({s_to_r.next()}),
+        LR1Set({l_to_r.next(), l_to_r_eq.next()}),
+        LR1Set({l_to_id.next(), l_to_id_eq.next()}),
+        LR1Set({s_to_l.next().next()}),
+        LR1Set({l_to_r.next().next(), l_to_r_eq.next().next()}),
+        LR1Set({r_to_l.next(), r_to_l_eq.next()}),
+        LR1Set({s_to_l.next().next().next()}),
+    )
+
+    g = Grammar((sp_production, s_production, l_production, r_production), Sp)
+    automata = LALRAutomata(g)
+    assert automata.states == set(states)
